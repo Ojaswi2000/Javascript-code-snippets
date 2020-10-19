e2d4f52cd25c836a216dddbf73fe6910e97613eb
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-10-20 00:51:40 +0530

    Add files via upload

---
+++ Javascript/Javascript numbers.js
@@ -0,0 +1,44 @@
+// This file will include error-free codes for basics of Javascript numbers.
+//Basics of arithmetic operations
+19+4                                     //addition
+23
+
+1.99999999999999999999999999999          //testing Javascript limits
+2
+
+13%6                                    //modulus operator
+1
+
+78-98                                   // subtraction
+-20
+
+8/4                                     //division
+2
+
+8//3
+8
+
+0/0                                     //testing NaN values
+NaN
+
+1+NaN
+NaN
+
+//The typeof operator
+typeof 4
+"number"
+typeof "Ojaswi"
+"string"
+typeof NaN                             //NaN is considered a part of the number family
+"number"
+
+//Basic arithmetic operations
+4+3*4/2
+10
+
+(13%5)**2
+9
+
+200+0/0
+NaN
+
