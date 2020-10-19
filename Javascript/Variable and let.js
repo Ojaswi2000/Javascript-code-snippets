1cae350fabe546401a148e73b43381344c8d9940
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-10-20 01:17:26 +0530

    added the file for let keyword

---
+++ Javascript/Variable and let.js
@@ -0,0 +1,47 @@
+//Variables are like labels for values so that we can use them later or refer back to them.
+let year=1985      //registering a variable 
+undefined
+
+year               //referring back to the year variable
+1985
+
+let a=34           //creating a variable with value of 34
+undefined
+
+let b=67          //creating a variable with value of 67
+undefined
+
+a
+34
+
+b
+67
+
+a+b             //summing a and b together
+101
+
+let c=a+b       //adding a and b together to store their result
+undefined
+
+c               //printing c
+101
+
+a+=1            //updating the variable a or by adding (a=a+1)
+35
+
+c               //checking for c=a+b after updating a and it still remains the same..Hence, no change occurs in the original value
+101
+
+//A small code snippet to understand the increment and decrement operators
+let score=0
+undefined
+score++
+0
+score
+1
+score*=6
+6
+score--
+6
+score
+5
