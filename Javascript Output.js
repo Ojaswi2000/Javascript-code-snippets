f8ca1426af048d49399db5526ab1b55bfe7c4d60
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-11-22 20:53:16 +0530

    added writing into console using innerHTML

---
+++ Javascript Output.js
@@ -0,0 +1,12 @@
+<html>
+    
+    <body>
+        <p>1. Writing usinh innerHTML</p>
+        <p id="hello"></p>
+        
+        <script>
+            document.getElementById('hello').innerHTML=5+7
+            
+        </script>
+    </body>
+</html>
