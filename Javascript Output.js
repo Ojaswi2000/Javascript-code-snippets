38c6ec373527d6f935c1c779c20fe6ba884c1eb1
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-11-22 20:56:51 +0530

    added document.write() and window.alert()

--- Javascript Output.js
+++ Javascript Output.js
@@ -1,12 +1,15 @@
 <html>
     
     <body>
-        <p>1. Writing usinh innerHTML</p>
+        <p>1. Writing using innerHTML</p>
+        <p>2. Writing using document.write()</p>
+        <p>3. Writing using window.alert()</p>
         <p id="hello"></p>
         
         <script>
             document.getElementById('hello').innerHTML=5+7
-            
+            document.write(5+7);
+            window.alert(5+7);
         </script>
     </body>
 </html>
