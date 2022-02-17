8fbf3535ceb733168a3ec323b6ca8f88b372bd91
Ojaswi Awasthi <ojaswiawasthi2025@gmail.com>
2022-02-18 01:38:42 +0530

    Update this keyword.js

--- this keyword.js
+++ this keyword.js
@@ -1,1 +1,8 @@
-console.log(Window);
+console.log(Window); // this return a window object on the screen
+
+function myfunc(){
+    var a=10;
+    var b=20;
+    console.log(this);
+}
+myfunc(); // this return a window object on the screen
