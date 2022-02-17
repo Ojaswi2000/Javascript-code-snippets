ec349915f99e62fdc1678197749c39a85eafe18c
Ojaswi Awasthi <ojaswiawasthi2025@gmail.com>
2022-02-18 01:51:00 +0530

    Update this keyword.js

--- this keyword.js
+++ this keyword.js
@@ -10,4 +10,17 @@ myfunc(); // this return a window object on the screen
 
 // variable becomes a property and function becomes a method in the console using this
 
-
+const servername={
+    name:"Discord",
+    getservername:function(){
+        console.log(this);
+    }
+}
+servername.getservername(); 
+// // The output will be:const servername={
+//     name:"Discord",
+//     getservername:function(){
+//         console.log(this);
+//     }
+// }
+// 
